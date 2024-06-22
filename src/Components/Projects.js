c5d41825022d8bfeaf86a40f83090ea1758acd2b
Manvith Chandra <96135784+ManvithChandra0@users.noreply.github.com>
2024-06-22 14:28:33 +0530

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,13 +9,24 @@ const Projects = () => {
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={Weather} projectName="Weather API Project using Spring Boot Microservices" />
-        <ProjectBox projectPhoto={Emotion} projectName="Real Time Emotion Detection Using CNN" />
-        <ProjectBox projectPhoto={Auto} projectName="Auto Mobile Management System Using Mern Stack" />
+        <ProjectBox 
+          projectPhoto={Weather} 
+          projectName="Weather API Project using Spring Boot Microservices" 
+          projectGithub="https://github.com/ManvithChandra0/WeatherApiProject"
+        />
+        <ProjectBox 
+          projectPhoto={Emotion} 
+          projectName="Real Time Emotion Detection Using CNN" 
+          projectGithub="https://github.com/ManvithChandra0/Real-Time-Emotion-Detection-Using-CNN"
+        />
+        <ProjectBox 
+          projectPhoto={Auto} 
+          projectName="Auto Mobile Management System Using Mern Stack" 
+          projectGithub="https://github.com/ManvithChandra0/MernAmsProject"
+        />
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Projects
